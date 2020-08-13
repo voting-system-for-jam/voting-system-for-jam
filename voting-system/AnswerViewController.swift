d1aa8c7f7715201d34e75b9432951068838c877d
ryota <s1811345@s.tsukba.ac.jp>
2020-08-13 21:12:01 +0900

    回答する画面の番号入力フォームを作成した

---
+++ voting-system/AnswerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AnswerViewController.swift
+//  voting-system
+//
+//  Created by Ryota Sato on 2020/08/13.
+//  Copyright © 2020 Ryota. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    
+    @IBOutlet weak var backviewcontroller_button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back_viewcontroller(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
