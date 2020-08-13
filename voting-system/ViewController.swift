ab33b21a52cdd57e2e0f1f3b7de3c9b45b3a6bad
ryota <s1811345@s.tsukba.ac.jp>
2020-08-13 14:43:54 +0900

    Initial Commit

---
+++ voting-system/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  voting-system
+//
+//  Created by Ryota Sato on 2020/08/13.
+//  Copyright © 2020 Ryota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
