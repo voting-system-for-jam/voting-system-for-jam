74a83a01ad6c867c73aed31bc6ab09c04c4e6ae0
Aiga <s1811337@u.tsukuba.ac.jp>
2020-08-27 17:33:49 +0900

    フォーム管理画面の作成をしました

--- voting-system/LoginViewController.swift
+++ voting-system/LoginViewController.swift
@@ -14,6 +14,8 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var make_accountButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
